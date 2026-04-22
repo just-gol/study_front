f22c47d85873804a81c024e4fbf1dea60d94e770
aabb <809073023@qq.com>
2026-04-22 22:12:43 +0800

    feat:  add npm01, .gitignore、package.json、http03.js 和 index.js

---
+++ Node/npm01/http03.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  let { method } = request;
+  let { pathname } = new URL(request.url, "http://127.0.0.1");
+  console.log(method);
+  console.log(pathname);
+})
+
+server.listen(9001, () => {
+  console.log("start");
+})
---
+++ Node/npm01/index.js
@@ -0,0 +1,5 @@
+const uniq = require("uniq");
+
+var arr = [1, 1, 2, 2, 3, 5]
+const result = uniq(arr);
+console.log(result);
