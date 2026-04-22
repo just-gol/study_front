5a98754b5a85d3500ede136fce17a2a15f2ae30e
aabb <809073023@qq.com>
2026-04-22 15:33:51 +0800

    feat: update http03 server  path

--- Node/http/http03.js
+++ Node/http/http03.js
@@ -1,10 +1,13 @@
 import http from "http";
+import path from "path";
 import url from 'url';
 
 
 const server = http.createServer((request, response) => {
-
-  response.end("end");
+  let { method } = request;
+  let { pathname } = new URL(request.url, "http://127.0.0.1");
+  console.log(method);
+  console.log(pathname);
 })
 
 server.listen(9001, () => {
